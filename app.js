9b996a53860b7e7769305f4ea57322197087cf2f
sofiaalessiod <eaa23sead@students.eaaa.dk>
2024-05-27 13:21:56 +0200

    si

--- app.js
+++ app.js
@@ -3,6 +3,34 @@
 window.addEventListener("load", initApp); // When the page is loaded, run initApp function
 
 // Function to initialize the Web App
-function initApp() {
+async function initApp() {
   console.log("initApp: app.js is running 🎉"); // Log to the console that the app is running
+  const posts = await getPosts();
+  console.log(posts);
 }
+
+async function getPosts() {
+  const response = await fetch(
+    "https://headless.cederdorff.dk/wp-json/wp/v2/posts?acf_format=standard"
+  );
+  const data = await response.json();
+  return data;
+}
+
+function displayPosts(posts) {
+  const postsList = document.querySelector("#posts-list");
+  for (const post of posts) {
+    postsList.insertAdjacentHTML(
+      "beforeend"
+      `
+      <li>${post.title.rendered}</li>
+    `
+    );
+  }
+}
+
+
+
+
+
+
