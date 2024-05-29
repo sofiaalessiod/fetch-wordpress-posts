23676d64c1f143d0d9b9aa8e66e3ecfaaf2a87c1
sofiaalessiod <eaa23sead@students.eaaa.dk>
2024-05-29 13:31:39 +0200

    ye

--- app.js
+++ app.js
@@ -7,17 +7,35 @@ async function initApp() {
   console.log("initApp: app.js is running 🎉"); // Log to the console that the app is running
   const posts = await getPosts();
   console.log(posts);
+  displayPostsGrid(posts);
 }
 
 async function getPosts() {
   const response = await fetch(
-    "https://headless.cederdorff.dk/wp-json/wp/v2/posts?acf_format=standard"
+    "https://headless.sofiaalessiod.dk/wp-json/wp/v2/posts?acf_format=standard"
   );
   const data = await response.json();
   return data;
 }
 
-function displayPosts(posts) {
+function displayPostsGrid(posts){
+  const postsGrid = document.querySelector("#posts-grid");
+
+  for (const post of posts) {
+    postsGrid.insertAdjacentHTML(
+      "beforeend",
+      /*html*/ `
+      <article class="grid-item">
+        <img src="${post.acf.image}" alt="${post.title.rendered}" />
+        <h2>${post.title.rendered}</h2>
+      </article>
+    `
+    );
+  }
+}
+
+/*
+ function displayPosts(posts) {
   const postsList = document.querySelector("#posts-list");
   for (const post of posts) {
     postsList.insertAdjacentHTML(
@@ -28,21 +46,5 @@ function displayPosts(posts) {
     );
   }
 }
-
-
-function displayPostsGrid(posts){
-  const postsGrid = document.querySelector("#posts-grid");
 }
-
-for (const post of posts) {
-  postsGrid.insertAdjacentHTML(
-    "beforeend",
-    /*html*/ `
-    <article class="grid-item">
-      <img src="${post.acf.image}" alt="${post.title.rendered}" />
-      <h2>${post.title.rendered}</h2>
-    </article>
-  `
-  );
-}
-
+*/
